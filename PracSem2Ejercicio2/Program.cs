193eed9dc2a55f6de5c553cc8c28be08ab779bf2
Levi <Computosala42022@ugb.edu.sv>
2022-07-29 10:08:06 -0600

    Primer programa

---
+++ PracSem2Ejercicio2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PracSem2Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaracion de variables 
+            string firstname = "Enrique";
+            String lastname = "Marenco";
+            int edad = 27;
+            //Concatenacion de variables 
+            var name = firstname + " " + lastname;
+            //Imprimir variables dentro del texto 
+            Console.WriteLine("Su nombre es: {0} y su edad es: {1}", name, edad);
+        }
+    }
+}
--- PracticaSemana2/Program.cs
+++ PracticaSemana2/Program.cs
@@ -6,7 +6,16 @@ namespace PracticaSemana2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declaracion de variables
+            int num1 = 5;
+            int num2 = 4;
+            var total = num1 + num2;
+
+            string name = "Enrique";
+            int edad = 27;
+            //Imprecion de datos
+            Console.WriteLine("La suma es: " + total);
+            Console.WriteLine(name + " " + edad);
         }
     }
 }
