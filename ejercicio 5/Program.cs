efaa14ddc7137df66bdd18d2ad4a36e527a11d54
Levi <Computosala42022@ugb.edu.sv>
2022-07-29 10:27:37 -0600

    ejercicio 4 y 5

---
+++ ejercicio 5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace ejercico4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introdusca su nombre: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("introdusca su edad");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("su nombre es: {0} y tiene {1} años de edad", name, edad);
+        }
+    }
+}
---
+++ ejercico4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ejercico4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Introdusca un numero: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("introdusca su edad");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("La suma de los numeros es: ", + (num1 + num2));
+            Console.WriteLine("La resta de los numeros es: ", +(num1 - num2));
+        }
+    }
+}
